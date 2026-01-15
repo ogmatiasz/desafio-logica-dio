653bd051feafeddcd032b05be0f360ef97a55ebf
ogmatiasz <tiagosantoro@gmail.com>
2026-01-14 22:31:59 -0300

    atualizado

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,74 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Olá, mundo!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+class Main {
+    public static void main(String[] args) {
+
+        Scanner expierence = new Scanner(System.in);
+
+        String heroiJava = "Java-man";
+        System.out.println("Digite o XP adquirido no momento:");
+        int xp = 1000;
+        int xpe = expierence.nextInt();
+
+        switch (xp){
+
+            case 1 :
+                if (xp <= 1000){
+                    System.out.println("O herói" + heroiJava + "tem um xp de" + xp);
+                }
+                else {
+                    System.out.println("O herói" + heroiJava + "tem um xp de" + xp + "e é bronze");
+
+                };
+                break;
+            case 2 :
+                if (xp <= 1001){
+                    System.out.println("O herói" + heroiJava + "tem um xp de" + xp);
+                }
+                else {
+                    System.out.println("O herói" + heroiJava + "tem um xp de" + xp);
+
+                }
+                break;
+            case 3 :
+                if (xp <= 3000){
+                    System.out.println("O herói" + heroiJava + "tem um xp de" + xp);
+                }
+                else {
+                    System.out.println("O herói" + heroiJava + "tem um xp de" + xp);
+
+                }
+                break;
+            case 4 :
+                if (xp <= 4000){
+                    System.out.println("O herói" + heroiJava + "tem um xp de" + xp);
+                }
+                else {
+                    System.out.println("O herói" + heroiJava + "tem um xp de" + xp);
+
+                }
+                break;
+            case 5 :
+                if (xp <= 5000){
+                    System.out.println("O herói" + heroiJava + "tem um xp de" + xp);
+                }
+                else {
+                    System.out.println("O herói" + heroiJava + "tem um xp de" + xp);
+
+                }
+                break;
+            default:
+
+
+
+        }
+
+        System.out.println("Digite o Xp adquirido no momento");
+
+
+
+
+
+
         }
     }
-}
--- src/Main2.java
+++ src/Main2.java
@@ -1,4 +1,60 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
-public class Main2 {
-}
+    public class Main2 {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Digite o nome do herói: ");
+            String nome = scanner.nextLine();
+
+            System.out.print("Digite a quantidade de XP do herói: ");
+            int xp = scanner.nextInt();
+
+            String nivel;
+            String level;
+            int levelup = 0;
+
+            // Convertendo XP em uma experiencia
+            if (xp < 1000) level = "ferro";
+            else if (xp <= 1001) level = "ferro";
+            else if (xp <= 2001) level = "bronze";
+            else if (xp <= 5001) level = "prata";
+            else if (xp <= 7001) level = "ouro";
+            else if (xp <= 9001) level = "platina";
+            else if (xp <= 10000)level = "ascendente";
+            else level = "radiante";
+
+            // Usando switch com base no level
+            switch (levelup) {
+                case 0:
+                    nivel = "Ferro";
+                    break;
+                case 1:
+                    nivel = "Bronze";
+                    break;
+                case 2:
+                    nivel = "Prata";
+                    break;
+                case 3:
+                    nivel = "Ouro";
+                    break;
+                case 4:
+                    nivel = "Platina";
+                    break;
+                case 5:
+                    nivel = "Ascendente";
+                    break;
+                case 6:
+                    nivel = "Imortal";
+                    break;
+                case 7:
+                    nivel = "Radiante";
+                    break;
+                default:
+                    nivel = "Desconhecido";
+            }
+
+            System.out.println("O Herói de nome " + nome + " está no nível de " + nivel);
+
+        }
+    }
